16580bb4ee8647275b26d2ffc395ffe7674352cb
Vinicius Alves <139510555+Vitenskapp@users.noreply.github.com>
2025-02-09 11:56:02 -0300

    Configuring H2 test database

--- src/main/java/com/alcode/spring_project/Entities/User.java
+++ src/main/java/com/alcode/spring_project/Entities/User.java
@@ -1,15 +1,22 @@
 package com.alcode.spring_project.Entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "user_table")
 public class User implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String phone;
