4573474c5397054259c5686ce23ff022668c2db2
Vinicius Alves <139510555+Vitenskapp@users.noreply.github.com>
2025-02-09 17:14:51 -0300

    Order entity implemented, fixed table names, database relations between entities

--- src/main/java/com/alcode/spring_project/Entities/User.java
+++ src/main/java/com/alcode/spring_project/Entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -22,6 +24,9 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
     public User() {
     }
 
@@ -31,6 +36,10 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public Long getId() {
