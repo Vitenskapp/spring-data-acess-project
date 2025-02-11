70405c4edf4cf3449c9caf5ed79a0a6b61ce9ad3
Vinicius Alves <139510555+Vitenskapp@users.noreply.github.com>
2025-02-11 19:30:13 -0300

    Implementing and testing enum OrderStatus and methods

--- src/main/java/com/alcode/spring_project/Config/TestConfiguration.java
+++ src/main/java/com/alcode/spring_project/Config/TestConfiguration.java
@@ -1,5 +1,6 @@
 package com.alcode.spring_project.Config;
 
+import com.alcode.spring_project.Entities.Enums.OrderStatus;
 import com.alcode.spring_project.Entities.Order;
 import com.alcode.spring_project.Entities.User;
 import com.alcode.spring_project.Repositories.OrderRepository;
@@ -30,9 +31,9 @@ public class TestConfiguration implements CommandLineRunner {
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "999999999", "123456");
         User user2 = new User(null, "Alex Green", "alex@gmail.com", "999999888", "321654");
 
-        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
